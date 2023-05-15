2958fc84d8d5df71e4eb9854d5409f7fcfb9954f
Matthew Lee <matthew@farlandlee.com>
2023-05-15 13:12:59 -0400

    simplify api calls

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseURL = process.env.NODE_ENV === 'production'? 'https://api.tworeporters.com' : 'http://localhost:3000'
+const baseURL = process.env.NODE_ENV === 'production'? '' : 'http://localhost:3000'
 const api = axios.create({
     baseURL: `${baseURL}/api`
 })
